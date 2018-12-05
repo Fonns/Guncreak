dd70a5132df0670dbc93c5490fb82180c34175ec
João Fonseca <joaofranciscovilarinho@gmail.com>
2018-12-05 21:13:24 +0000

    Initial Commit

---
+++ Source/Guncreak/Guncreak.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "Guncreak.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Guncreak, "Guncreak" );
+ 
