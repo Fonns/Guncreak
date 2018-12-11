0ebec3a2a7e664246ca1f4705a8bab6455b84344
João Fonseca <joaofranciscovilarinho@gmail.com>
2018-12-11 01:03:33 +0000

    Reset

---
+++ Source/Guncreak/GuncreakGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GuncreakGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUNCREAK_API AGuncreakGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
